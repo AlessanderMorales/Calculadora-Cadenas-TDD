7d5d6722559f03588ebfb33254e23007282296be
AlessanderMorales <alessmoralesmo@gmail.com>
2026-05-27 23:54:10 -0400

    Ciclo 07: acepta delimitador personalizado simple

--- src/stringCalculator.test.js
+++ src/stringCalculator.test.js
@@ -4,24 +4,28 @@ describe("Calculadora de cadenas", () => {
   test("cadena vacia retorna 0", () => {
     expect(add("")).toBe(0);
   });
-});
 
-test("cadena con un solo numero retorna ese numero", () => {
-  expect(add("2")).toBe(2);
-});
+  test("cadena con un solo numero retorna ese numero", () => {
+    expect(add("2")).toBe(2);
+  });
 
-test("dos numeros separados por coma retornan la suma", () => {
-  expect(add("1,2")).toBe(3);
-});
+  test("dos numeros separados por coma retornan la suma", () => {
+    expect(add("1,2")).toBe(3);
+  });
 
-test("varios numeros separados por coma retornan la suma", () => {
-  expect(add("1,2,3,4")).toBe(10);
-});
+  test("varios numeros separados por coma retornan la suma", () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
 
-test("numeros separados por guion retornan la suma", () => {
-  expect(add("1-2-3")).toBe(6);
-});
+  test("numeros separados por guion retornan la suma", () => {
+    expect(add("1-2-3")).toBe(6);
+  });
 
-test("numeros separados por coma y guion retornan la suma", () => {
-  expect(add("1-2,3")).toBe(6);
+  test("numeros separados por coma y guion retornan la suma", () => {
+    expect(add("1-2,3")).toBe(6);
+  });
+
+  test("delimitador personalizado retorna la suma", () => {
+    expect(add("//[;] 6;7;4")).toBe(17);
+  });
 });
